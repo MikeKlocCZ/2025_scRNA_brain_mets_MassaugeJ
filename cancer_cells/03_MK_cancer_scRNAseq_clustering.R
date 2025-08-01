2a3cf871efe9208382e26f893aa36e058af8782c
michal <michal@dyn-3-5-197.mobile.unibas.ch>
2025-08-01 08:53:33 +0200

    Cancer folder done

--- cancer_cells/03_MK_cancer_scRNAseq_clustering.R
+++ cancer_cells/03_MK_cancer_scRNAseq_clustering.R
@@ -10,9 +10,9 @@ pkg <- c("SingleCellExperiment",  "edgeR", "scuttle", "bluster",
          "scater", "scran", "umap", "tidyverse", "cowplot", "batchelor",
          "BiocParallel", "BiocSingular", "pheatmap")
 
-pkg <- setdiff(pkg, rownames(installed.packages()))
+pkg.install <- setdiff(pkg, rownames(installed.packages()))
 if (length(pkg) > 0) {
-  BiocManager::install(pkg)
+  BiocManager::install(pkg.install)
 }
 ## Load packages
 suppressPackageStartupMessages({
